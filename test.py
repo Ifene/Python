451cd8611bd7165b88a630510975f0479932a9e8
Hurst <hursts@wwt.com>
2022-06-07 10:58:46 -0500

    Practice with Numbers

--- test.py
+++ test.py
@@ -1,3 +1,38 @@
 # practice with Python
 
-print('hello')
+# practice with numbers
+
+def numtest1():
+    print(1+1)
+    print(2*2)
+    print(7/4)
+    print(50%5)
+    print(23%2)
+    print(2**3)
+    print(2+10*10+3)
+    print((2+10)*(10+3))
+
+
+# Practicing store ideas
+long_sword = 50
+
+hand_axe = 10
+
+total_purchase = long_sword + hand_axe
+
+value_long_sword = f'{long_sword} Gold'
+
+#value_long_sword = str(long_sword)+' Gold'
+
+print(total_purchase)
+
+print(value_long_sword)
+
+
+
+# Intro to strings and Indexing
+
+
+
+
+
