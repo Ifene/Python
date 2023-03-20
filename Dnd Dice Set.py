82754dc419e9b7e6d4efb597988f7e5f5e1454c4
Hurst <hursts@wwt.com>
2023-03-20 11:01:39 -0500

    Began creating a full Dnd dice roller

---
+++ Dnd Dice Set.py
@@ -0,0 +1,25 @@
+import random
+
+#D4 Dice
+D4 = random.randint(0, 4)
+
+#D6 Dice
+D6 = random.randint(0, 6)
+
+#D8 Dice
+D8 = random.randint(0, 8)
+
+#D10 Dice
+D10 = random.randint(0, 10)
+
+#D12 Dice
+D12 = random.randint(0, 12)
+
+#D20 Dice
+D20 = random.randint(0, 20)
+
+
+Dice_Amount = input("Enter the Dice you'd like to roll. \n")
+
+
+
